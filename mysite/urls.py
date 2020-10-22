962e74705193a94039cb7e4ffee12c755935a843
t18cs059 <t18cs059@gmail.com>
2020-10-22 16:01:43 +0900

    urls.py add urlpatterns

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('plus/', include('plus.urls')),
+    path('', RedirectView.as_view(url='/plus/')),
     path('admin/', admin.site.urls),
 ]
